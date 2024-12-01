5e4904563eb336d457c542955652e73b8d549ec3
Mahfud Portofolio <75352651+mahfudinkerja@users.noreply.github.com>
2024-12-01 16:17:43 +0700

    Delete VSCode directory

--- VSCode/Cypress5.0/cypress.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-    e2e: {
-        pageLoadTimeout: 120000, // Tunggu hingga 2 menit
-        defaultCommandTimeout: 10000, // Timeout default untuk perintah lainnya
-    },
-});
